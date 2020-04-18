d8b88fcaafe31d0f30f7c5ccf48630eaf4b26c30
Caner Demirci <caner.demirci@outlook.com>
2020-04-18 20:55:17 +0300

    Add files via upload

---
+++ Assets/Script/Rope.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    public Rigidbody2D hook;
+    public GameObject linkPrefab;
+    public int links = 7;
+    public Weight weight;
+
+    void Start()
+    {
+        GenerateRope();
+    }
+
+    void GenerateRope()
+    {
+        Rigidbody2D previousRB = hook;
+
+        for (int i=0; i<links; i++)
+        {
+            GameObject link = Instantiate(linkPrefab, transform);
+            HingeJoint2D joint = link.GetComponent<HingeJoint2D>();
+            joint.connectedBody = previousRB;
+
+            if (i < links-1)
+            {
+                previousRB = link.GetComponent<Rigidbody2D>();
+            }
+            else
+            {
+                weight.ConnectRopeEnd(link.GetComponent<Rigidbody2D>());
+            }
+        }
+    }
+}
